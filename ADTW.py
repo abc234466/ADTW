5c1af8eac38057124b288f2178f4876c6e5d105f
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2018-04-12 13:45:52 +0800

    Add Origin_DTW
    Next:
    design ADTW

--- ADTW.py
+++ ADTW.py
@@ -2,5 +2,27 @@
 First comment
 
 '''
+import numpy as np # including matrix
+import math # including inf
+TS =[9,3,8,7,4,1,3,2,1,7]
+ComparedTS=[18,2,9,8,8,5,4,2,1,5]
+
+# Trim the label of the time series
+Trim_TS=TS[1:]
+Trim_ComparedTS=ComparedTS[1:]
+
+#Origin DTW
+
+def originDTW(ts1, ts2):
+    matrix_DTW = np.array([[math.inf for i in range(len(ts1) + 1)] for i in range(len(ts2) + 1)])
+    matrix_DTW[0][0] = 0
+
+    for i in range(1, len(ts1)+1):
+        for j in range(1, len(ts2)+1):
+            distance = abs(ts1[i-1] - ts2[j-1])
+            matrix_DTW[i][j] = distance + min(matrix_DTW[i-1][j], matrix_DTW[i][j-1], matrix_DTW[i-1, j-1])
+    return matrix_DTW[i][j]
+
+print(originDTW(Trim_TS, Trim_ComparedTS))
 
 
