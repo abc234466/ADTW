5c6c98ee7c238e7120932822ee6505587d91429b
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2018-04-18 18:58:58 +0800

    完成ADTW 暴力法

--- ADTW.py
+++ ADTW.py
@@ -33,7 +33,7 @@ def originDTW(ts1, ts2):
     return matrix_DTW[i][j]
 
 
-print(originDTW(Trim_TS, Trim_ComparedTS))
+#print(originDTW(Trim_TS, Trim_ComparedTS))
 
 
 def adaptiveWindowDTW(ts1, ts2):
@@ -67,55 +67,63 @@ def adaptiveWindowDTW(ts1, ts2):
             return matrix_ADTW[ts1_length-1][ts2_length-1]
 
         else:
+            if comparedIncreasingQueue:
+                comparedIncreasingQueue.pop(0)
             if ts1_index + 1 < ts1_length and ts2_index + 1 < ts2_length:
                 distance_dia = abs(ts1[ts1_index + 1] - ts2[ts2_index + 1])
                 if current_minimum_distance + distance_dia < matrix_ADTW[ts1_index + 1][ts2_index + 1]:
-                    matrix_ADTW[ts1_index + 1][ts2_index + 1] = current_minimum_distance + distance_dia
+                    matrix_ADTW[ts1_index + 1][ts2_index + 1] = matrix_ADTW[ts1_index][ts2_index] + distance_dia
                     comparedIncreasingQueue = updateQueue(comparedIncreasingQueue, current_minimum_distance + distance_dia, ts1_index + 1, ts2_index + 1)
             if ts1_index + 1 < ts1_length:
                 distance_down = abs(ts1[ts1_index + 1] - ts2[ts2_index])
                 if current_minimum_distance + distance_down < matrix_ADTW[ts1_index + 1][ts2_index]:
-                    matrix_ADTW[ts1_index + 1][ts2_index] = current_minimum_distance + distance_down
+                    matrix_ADTW[ts1_index + 1][ts2_index] = matrix_ADTW[ts1_index][ts2_index] + distance_down
                     comparedIncreasingQueue = updateQueue(comparedIncreasingQueue, current_minimum_distance + distance_down, ts1_index + 1, ts2_index)
             if ts2_index + 1 < ts2_length:
                 distance_right = abs(ts1[ts1_index] - ts2[ts2_index + 1])
                 if current_minimum_distance + distance_right < matrix_ADTW[ts1_index][ts2_index + 1]:
-                    matrix_ADTW[ts1_index][ts2_index + 1] = current_minimum_distance + distance_right
+                    matrix_ADTW[ts1_index][ts2_index + 1] = matrix_ADTW[ts1_index][ts2_index] + distance_right
                     comparedIncreasingQueue = updateQueue(comparedIncreasingQueue, current_minimum_distance + distance_right, ts1_index, ts2_index + 1)
             distance_minimum = min(distance_dia, distance_down, distance_right)
 
             current_minimum_distance = current_minimum_distance + distance_minimum
-            ts1_index = comparedIncreasingQueue[0][1][0]
-            ts2_index = comparedIncreasingQueue[0][1][1]
-            firstQueue = comparedIncreasingQueue[0]
-
-            #remove duplicate cells
-            while firstQueue in comparedIncreasingQueue:
-                comparedIncreasingQueue.remove(firstQueue)
-
+            ts1_index = comparedIncreasingQueue[0][1]
+            ts2_index = comparedIncreasingQueue[0][2]
             # sort the queue
             # sorted(comparedIncreasingQueue, key = itemgetter(0))
             # the first step
 
 
 def updateQueue(queue, inputValue, ts1_index, ts2_index):
-    # check if list is empty
-    if queue:
-        if inputValue >= queue[len(queue)-1][0]:
-            queue.insert(len(queue), [inputValue, [ts1_index, ts2_index]])
-            return queue
-        else:
-            for i in range(0, len(queue)):
-                if inputValue < queue[i][0]:
-                    queue.insert(i, [inputValue, [ts1_index,ts2_index]])
-                if inputValue == queue[i][0]
-                elif i == len(queue) - 1:
-                    queue.insert(i+1, [inputValue, [ts1_index,ts2_index]])
-            return queue
-    # append value at the first time
-    else:
-        queue.append([inputValue, [ts1_index, ts2_index]])
+    insertList = [inputValue, ts1_index, ts2_index]
+    if insertList in queue:
         return queue
+    else:
+        queue.insert(0, insertList)
+        queue = sorted(queue, key=itemgetter(0))
+        return queue
+    #     try:
+    #         queue.insert(queue.index(inputValue), insertList)
+    #     except:
+    #         firstValueList = queue[:][0]
+    #         for i in range(0, len(firstValueList)):
+    #             if inputValue <= firstValueList[i]:
+    #                 queue.insert(i, insertList)
+    #
+    #
+    #
+    #
+    # if queue:
+    #     if inputValue >= queue[len(queue)-1][0]:
+    #         queue.insert(len(queue), [inputValue, [ts1_index, ts2_index]])
+    #         return queue
+    #     else:
+    #         for i in range(0, len(queue)):
+    #             if inputValue < queue[i][0]:
+    #                 queue.insert(i, [inputValue, [ts1_index,ts2_index]])
+    #             elif i == len(queue) - 1:
+    #                 queue.insert(i+1, [inputValue, [ts1_index,ts2_index]])
+    #         return queue
 
 
 print(adaptiveWindowDTW(Trim_TS, Trim_ComparedTS))
