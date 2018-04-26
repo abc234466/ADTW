c16f2b96e6ab7e8d2eae9c4b370cce43b428a609
DESKTOP-D2P461F\Bo Xian Chen <abc2344666@gmail.com>
2018-04-26 15:11:41 +0800

    還需要修改的版本.....

--- ADTW.py
+++ ADTW.py
@@ -111,7 +111,7 @@ def adaptiveWindowDTW(ts1, ts2):
     ts1_length = len(ts1)  # m
     ts2_length = len(ts2)  # n
 
-    comparedIncreasingQueue = []
+    comparedIncreasingQueue = [[matrix_ADTW[ts1_index][ts2_index], 0, 0]]
 
     # comparedIncreasingQueueIndex = []
 
@@ -130,75 +130,70 @@ def adaptiveWindowDTW(ts1, ts2):
             # print(matrix_ADTW)
             return matrix_ADTW[ts1_length - 1][ts2_length - 1]
         else:
-            if comparedIncreasingQueue:
-                comparedIncreasingQueue.pop(0)
+
+            comparedIncreasingQueue.pop(0)
+            # right direction
             if ts1_index + 1 < ts1_length and ts2_index + 1 < ts2_length:
                 distance_dia = abs(ts1[ts1_index + 1] - ts2[ts2_index + 1])
-                if current_minimum_distance + distance_dia < matrix_ADTW[ts1_index + 1][ts2_index + 1]:
-                    matrix_ADTW[ts1_index + 1][ts2_index + 1] = matrix_ADTW[ts1_index][ts2_index] + distance_dia
-                    comparedIncreasingQueue = updateQueue(comparedIncreasingQueue,
-                                                          matrix_ADTW[ts1_index][ts2_index] + distance_dia,
-                                                          ts1_index + 1, ts2_index + 1)
+
+                cumulativeDia = matrix_ADTW[ts1_index][ts2_index] + distance_dia
+                if cumulativeDia < matrix_ADTW[ts1_index + 1][ts2_index + 1]:
+                    matrix_ADTW[ts1_index + 1][ts2_index + 1] = cumulativeDia
+                    insertList = [cumulativeDia, ts1_index, ts2_index]
+                    if insertList not in comparedIncreasingQueue:
+                        comparedIncreasingQueue.insert(0, insertList)
+
+            # down direction
             if ts1_index + 1 < ts1_length:
                 distance_down = abs(ts1[ts1_index + 1] - ts2[ts2_index])
-                if current_minimum_distance + distance_down < matrix_ADTW[ts1_index + 1][ts2_index]:
-                    matrix_ADTW[ts1_index + 1][ts2_index] = matrix_ADTW[ts1_index][ts2_index] + distance_down
-                    comparedIncreasingQueue = updateQueue(comparedIncreasingQueue,
-                                                          matrix_ADTW[ts1_index][ts2_index] + distance_down,
-                                                          ts1_index + 1, ts2_index)
+
+                cumulativeDown = matrix_ADTW[ts1_index][ts2_index] + distance_down
+                if cumulativeDown < matrix_ADTW[ts1_index + 1][ts2_index]:
+                    matrix_ADTW[ts1_index + 1][ts2_index] = cumulativeDown
+                    insertList = [cumulativeDown, ts1_index, ts2_index]
+                    if insertList not in comparedIncreasingQueue:
+                        comparedIncreasingQueue.insert(0, insertList)
+
+            #right direction
             if ts2_index + 1 < ts2_length:
                 distance_right = abs(ts1[ts1_index] - ts2[ts2_index + 1])
-                if current_minimum_distance + distance_right < matrix_ADTW[ts1_index][ts2_index + 1]:
-                    matrix_ADTW[ts1_index][ts2_index + 1] = matrix_ADTW[ts1_index][ts2_index] + distance_right
-                    comparedIncreasingQueue = updateQueue(comparedIncreasingQueue,
-                                                          matrix_ADTW[ts1_index][ts2_index] + distance_right, ts1_index,
-                                                          ts2_index + 1)
+
+                cumulativeRight = matrix_ADTW[ts1_index][ts2_index] + distance_right
+                if cumulativeRight < matrix_ADTW[ts1_index][ts2_index + 1]:
+                    matrix_ADTW[ts1_index][ts2_index + 1] = cumulativeRight
+
+                    insertList = [cumulativeRight, ts1_index, ts2_index]
+                    if insertList not in comparedIncreasingQueue:
+                        comparedIncreasingQueue.insert(0, insertList)
+
             distance_minimum = min(distance_dia, distance_down, distance_right)
 
             current_minimum_distance = current_minimum_distance + distance_minimum
+            # sort the queue
+            sorted(comparedIncreasingQueue, key=itemgetter(0))
+            print(comparedIncreasingQueue)
             ts1_index = comparedIncreasingQueue[0][1]
             ts2_index = comparedIncreasingQueue[0][2]
-            # sort the queue
-            # sorted(comparedIncreasingQueue, key = itemgetter(0))
+
             # the first step
 
 
 def updateQueue(queue, inputValue, ts1_index, ts2_index):
-    insertList = [inputValue, ts1_index, ts2_index]
     if insertList in queue:
         return queue
     else:
         queue.insert(0, insertList)
         queue = sorted(queue, key=itemgetter(0))
         return queue
-        #     try:
-        #         queue.insert(queue.index(inputValue), insertList)
-        #     except:
-        #         firstValueList = queue[:][0]
-        #         for i in range(0, len(firstValueList)):
-        #             if inputValue <= firstValueList[i]:
-        #                 queue.insert(i, insertList)
-        #
-        #
-        #
-        #
-        # if queue:
-        #     if inputValue >= queue[len(queue)-1][0]:
-        #         queue.insert(len(queue), [inputValue, [ts1_index, ts2_index]])
-        #         return queue
-        #     else:
-        #         for i in range(0, len(queue)):
-        #             if inputValue < queue[i][0]:
-        #                 queue.insert(i, [inputValue, [ts1_index,ts2_index]])
-        #             elif i == len(queue) - 1:
-        #                 queue.insert(i+1, [inputValue, [ts1_index,ts2_index]])
-        #         return queue
+
 
 def quicksort(queue):
-    if len(queue)<=1:
+    if len(queue) <= 1:
         return queue
-    midcell = queue[len(queue)//2]
-    return quicksort([ i for i in queue if i < midcell]) + [midcell] + quicksort([ i for i in queue if i > midcell])
+    midcell = queue[len(queue) // 2]
+    return quicksort([i for i in queue if i < midcell]) + [midcell] + quicksort([i for i in queue if i > midcell])
+
+
 #
 print(adaptiveWindowDTW(Trim_TS, Trim_ComparedTS))
 # print(time.time() - start_time)
